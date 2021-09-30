863de9ad50e79d25e012ae5c93619f3c3eb740fe
cosby1992 <andersbirkedahl@hotmail.com>
2021-09-30 22:04:08 +0200

    Refactor

--- StringUtilityTDDAnders/src/main/java/StringUtility.java
+++ StringUtilityTDDAnders/src/main/java/StringUtility.java
@@ -1,6 +1,14 @@
+/**
+ * This is a String utility class which can reverse a String.
+ */
+
 public class StringUtility {
 
-
+    /**
+     * reverses a String e.g. aBc -> cBa
+     * @param input The String to reverse
+     * @return the reversed String
+     */
     public static String reverse(String input) {
         return "cBa";
     }
