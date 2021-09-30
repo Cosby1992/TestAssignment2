83cbe22f65d85db9247904a0e3ee4b427e5e4084
cosby1992 <andersbirkedahl@hotmail.com>
2021-09-30 22:26:28 +0200

    refactor

--- StringUtilityTDDAnders/src/main/java/StringUtility.java
+++ StringUtilityTDDAnders/src/main/java/StringUtility.java
@@ -12,20 +12,26 @@ public class StringUtility {
     public static String reverse(String input) {
 
         char[] chars = input.toCharArray();
+
+        // pointers to keep track of left and right position in char array
         int leftPointer = 0;
         int rightPointer = chars.length-1;
 
         while(leftPointer < rightPointer){
 
+            // flip left and right letter (starting from the outside)
             char temp = chars[leftPointer];
             chars[leftPointer] = chars[rightPointer];
             chars[rightPointer] = temp;
 
+            // move the pointers one place towards center of array
             leftPointer++;
             rightPointer--;
 
+            // keep going until the pointers meet (or overlap)
         }
 
+        // return the reversed message
         return new String(chars);
     }
 }
