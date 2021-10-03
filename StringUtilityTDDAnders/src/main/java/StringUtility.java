7ec111a72ecde4af692b4dab859e4fb07fca6d3b
cosby1992 <andersbirkedahl@hotmail.com>
2021-10-03 22:15:17 +0200

    Small refactor (removed old code)

--- StringUtilityTDDAnders/src/main/java/StringUtility.java
+++ StringUtilityTDDAnders/src/main/java/StringUtility.java
@@ -26,7 +26,7 @@ public class StringUtility {
         // Go through the input array and place each char in the output array
         while(nextCharPointer < inputCharArray.length){
 
-            // Find out how many char[] places a char takes up
+            // Find out how many char[] indexes a char takes up
             // This is purely to support unicode characters like
             // smileys and special characters (e.g. chinese letters)
             int characterLength =  Character.charCount(Character.codePointAt(inputCharArray, nextCharPointer));
@@ -49,27 +49,6 @@ public class StringUtility {
         // return the reversed message
         return new String(reversedCharArray);
 
-        //        Previous solution
-        //
-        //        char[] chars = input.toCharArray();
-        //
-        //        // pointers to keep track of left and right position in char array
-        //        int leftPointer = 0;
-        //        int rightPointer = chars.length-1;
-        //
-        //        while(leftPointer < rightPointer){
-        //
-        //            // flip left and right letter (starting from the outside)
-        //            char temp = chars[leftPointer];
-        //
-        //            chars[leftPointer] = chars[rightPointer];
-        //            chars[rightPointer] = temp;
-        //
-        //            // move the pointers one place towards center of array
-        //            leftPointer++;
-        //            rightPointer--;
-        //
-        //            // keep going until the pointers meet (or overlap)
     }
 
 }
