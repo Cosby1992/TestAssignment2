b083883c6ac4eea822ec990c1aa45861b66ae5bf
cosby1992 <andersbirkedahl@hotmail.com>
2021-10-03 23:14:48 +0200

    Small refactor and Test implmented a -> A

--- StringUtilityTDDAnders/src/test/java/StringUtilityCapitalizeTest.java
+++ StringUtilityTDDAnders/src/test/java/StringUtilityCapitalizeTest.java
@@ -1,6 +1,17 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test Scope
+ * Class: StringUtility
+ * Method: public String capitalize(String input)
+ *
+ * Test framework
+ * Name: junit-jupiter
+ * Version: 5.8.1
+ *
+ * @author Anders
+ */
 public class StringUtilityCapitalizeTest {
 
     @Test
@@ -10,7 +21,15 @@ public class StringUtilityCapitalizeTest {
         String expected = null;
 
         assertEquals(expected, actual);
+    }
 
+    @Test
+    public void capitalize_a_A() {
+        String input = "a";
+        String actual = StringUtility.capitalize(input);
+        String expected = "A";
+
+        assertEquals(expected, actual);
     }
 
 }
--- StringUtilityTDDAnders/src/test/java/StringUtilityReverseStringTest.java
+++ StringUtilityTDDAnders/src/test/java/StringUtilityReverseStringTest.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author Anders
  */
-
 public class StringUtilityReverseStringTest {
 
     @Test
