bcf78641a9c44000687d63965965f3b17413cf45
cosby1992 <andersbirkedahl@hotmail.com>
2021-10-04 23:36:43 +0200

    refactor

--- StringUtilityTDDAnders/src/test/java/BowlingGameTest.java
+++ StringUtilityTDDAnders/src/test/java/BowlingGameTest.java
@@ -3,34 +3,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
+    private void rollMany(BowlingGame game, int iterations, int points) {
+        for (int i = 0; i < iterations; i++) {
+            game.roll(points);
+        }
+    }
+
     @Test
     public void getScore_gameOfZero_0() {
         BowlingGame game = new BowlingGame();
-
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
-
+        rollMany(game, 20, 0);
         int actual = game.getScore();
         int expected = 0;
-
         assertEquals(expected, actual);
-
     }
 
     @Test
     public void getScore_gameOfOnes_20() {
         BowlingGame game = new BowlingGame();
-
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
-
+        rollMany(game, 20, 0);
         int actual = game.getScore();
         int expected = 20;
-
         assertEquals(expected, actual);
-
     }
 
 }
