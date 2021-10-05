5ec01a988b892ebe2d3e89b68eb00b44a52ce2cd
cosby1992 <andersbirkedahl@hotmail.com>
2021-10-05 13:56:14 +0200

    Test passed

--- StringUtilityTDDAnders/src/main/java/BowlingGame.java
+++ StringUtilityTDDAnders/src/main/java/BowlingGame.java
@@ -1,11 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
-    int score = 0;
+    int[] scores = new int[21];
+    int roll = 0;
 
     public void roll(int i) {
-        score += i;
+        scores[roll++] = i;
     }
 
     public int getScore() {
-        return score;
+
+        int totalScore = 0;
+        int priviousScore = 0;
+
+        for (int activeFrameIndex = 0; activeFrameIndex < scores.length-1; activeFrameIndex += 2) {
+            int score = scores[activeFrameIndex] + scores[activeFrameIndex + 1];
+
+            // account for spare
+            if (priviousScore == 10) {
+                totalScore += scores[activeFrameIndex];
+            }
+
+            priviousScore = score;
+
+            totalScore += score;
+            System.out.println(totalScore);
+        }
+
+        return totalScore;
     }
 }
