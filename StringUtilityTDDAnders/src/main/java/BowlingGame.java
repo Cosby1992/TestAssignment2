91e34f4107906e8d5ad005212598daeb2063c86c
cosby1992 <andersbirkedahl@hotmail.com>
2021-10-06 12:03:13 +0200

    refactor

--- StringUtilityTDDAnders/src/main/java/BowlingGame.java
+++ StringUtilityTDDAnders/src/main/java/BowlingGame.java
@@ -9,6 +9,19 @@ public class BowlingGame {
         scores[roll++] = i;
     }
 
+    private boolean isSpare(int frameIndex) {
+        return scores[frameIndex] + scores[frameIndex + 1] == 10 && !isStrike(frameIndex);
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return scores[frameIndex] == 10;
+    }
+
+    private boolean isLastFrame(int frameIndex) {
+        return !(frameIndex < 18);
+    }
+
+
     public int getScore() {
 
         int totalScore = 0;
@@ -19,23 +32,22 @@ public class BowlingGame {
             // in any case the first two pins of the frame give their score
             totalScore += scores[activeFrameIndex] + scores[activeFrameIndex + 1];
 
-            if(activeFrameIndex < 18) {
+            if (!isLastFrame(activeFrameIndex)) {
                 // case for normal frame (two rolls)
 
 
-                if(scores[activeFrameIndex] == 10){
+                if (isStrike(activeFrameIndex)) {
                     // Case for strike
 
                     // case next roll is also a strike
-                    if (scores[getNextFrameIndex(activeFrameIndex)] == 10) {
+                    if (isStrike(getNextFrameIndex(activeFrameIndex))) {
                         totalScore += scores[getNextFrameIndex(activeFrameIndex)] + scores[getNextFrameIndex(getNextFrameIndex(activeFrameIndex))];
                     } else {
                         totalScore += scores[getNextFrameIndex(activeFrameIndex)] + scores[getNextFrameIndex(activeFrameIndex + 1)];
                     }
 
 
-
-                } else if (scores[activeFrameIndex] + scores[activeFrameIndex + 1] == 10){
+                } else if (isSpare(activeFrameIndex)) {
                     // Case for spare
                     totalScore += scores[getNextFrameIndex(activeFrameIndex)];
 
@@ -44,49 +56,10 @@ public class BowlingGame {
             } else {
                 // case for extended frame (10th frame)
 
-                totalScore += scores[activeFrameIndex+2];
+                totalScore += scores[activeFrameIndex + 2];
 
 
             }
-
-
-
-
-
-//            int score = scores[activeFrameIndex] + scores[activeFrameIndex + 1];
-//
-//                // add two in a row strike bonus
-//            if (activeFrameIndex > 3 && scores[activeFrameIndex - 2] == 10 && scores[activeFrameIndex - 4] == 10){
-//                totalScore += 10;
-//            } else if (activeFrameIndex > 0 && scores[activeFrameIndex - 2] == 10) {
-//                // account for strike
-//                totalScore += scores[activeFrameIndex] + scores[activeFrameIndex + 1];
-//            } else if (previousFrameScore == 10) {
-//                // account for spare
-//                totalScore += scores[activeFrameIndex];
-//            }
-//
-//            if (activeFrameIndex == 18) { // if it's the tenth frame of the game
-//                System.out.println("Total start -> " + totalScore);
-//                if (scores[activeFrameIndex] == 10 && scores[activeFrameIndex + 1] == 10) {
-//                    totalScore += scores[activeFrameIndex + 2];
-//                } if (scores[activeFrameIndex] == 10) {
-//                    totalScore += scores[activeFrameIndex + 1] + scores[activeFrameIndex + 2];
-//                } else if ((scores[activeFrameIndex] + scores[activeFrameIndex + 1] == 10)) {
-//                    totalScore += scores[activeFrameIndex + 2];
-//                } else if (scores[activeFrameIndex + 1] == 10) {
-//                    totalScore += scores[activeFrameIndex + 2];
-//                }
-//
-//                totalScore += scores[activeFrameIndex + 2];
-//
-//                System.out.println("Total end -> " + totalScore);
-//
-//            }
-//
-//            previousFrameScore = score;
-//
-//            totalScore += score;
         }
 
         return totalScore;
