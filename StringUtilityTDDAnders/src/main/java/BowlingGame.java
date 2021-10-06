99dd9692fdb15308193f8d204cba6c7989843b75
cosby1992 <andersbirkedahl@hotmail.com>
2021-10-06 11:55:46 +0200

    Test passed

--- StringUtilityTDDAnders/src/main/java/BowlingGame.java
+++ StringUtilityTDDAnders/src/main/java/BowlingGame.java
@@ -12,28 +12,87 @@ public class BowlingGame {
     public int getScore() {
 
         int totalScore = 0;
-        int priviousScore = 0;
+        int previousFrameScore = 0;
 
         for (int activeFrameIndex = 0; activeFrameIndex < scores.length-1; activeFrameIndex += 2) {
 
-            int score = scores[activeFrameIndex] + scores[activeFrameIndex + 1];
+            // in any case the first two pins of the frame give their score
+            totalScore += scores[activeFrameIndex] + scores[activeFrameIndex + 1];
 
-                // add two in a row strike bonus
-            if (activeFrameIndex > 3 && scores[activeFrameIndex - 2] == 10 && scores[activeFrameIndex - 4] == 10){
-                totalScore += 10;
-            } else if (activeFrameIndex > 0 && scores[activeFrameIndex - 2] == 10) {
-                // account for strike
-                totalScore += scores[activeFrameIndex] + scores[activeFrameIndex + 1];
-            } else if (priviousScore == 10) {
-                // account for spare
-                totalScore += scores[activeFrameIndex];
+            if(activeFrameIndex < 18) {
+                // case for normal frame (two rolls)
+
+
+                if(scores[activeFrameIndex] == 10){
+                    // Case for strike
+
+                    // case next roll is also a strike
+                    if (scores[getNextFrameIndex(activeFrameIndex)] == 10) {
+                        totalScore += scores[getNextFrameIndex(activeFrameIndex)] + scores[getNextFrameIndex(getNextFrameIndex(activeFrameIndex))];
+                    } else {
+                        totalScore += scores[getNextFrameIndex(activeFrameIndex)] + scores[getNextFrameIndex(activeFrameIndex + 1)];
+                    }
+
+
+
+                } else if (scores[activeFrameIndex] + scores[activeFrameIndex + 1] == 10){
+                    // Case for spare
+                    totalScore += scores[getNextFrameIndex(activeFrameIndex)];
+
+                }
+
+            } else {
+                // case for extended frame (10th frame)
+
+                totalScore += scores[activeFrameIndex+2];
+
+
             }
 
-            priviousScore = score;
 
-            totalScore += score;
+
+
+
+//            int score = scores[activeFrameIndex] + scores[activeFrameIndex + 1];
+//
+//                // add two in a row strike bonus
+//            if (activeFrameIndex > 3 && scores[activeFrameIndex - 2] == 10 && scores[activeFrameIndex - 4] == 10){
+//                totalScore += 10;
+//            } else if (activeFrameIndex > 0 && scores[activeFrameIndex - 2] == 10) {
+//                // account for strike
+//                totalScore += scores[activeFrameIndex] + scores[activeFrameIndex + 1];
+//            } else if (previousFrameScore == 10) {
+//                // account for spare
+//                totalScore += scores[activeFrameIndex];
+//            }
+//
+//            if (activeFrameIndex == 18) { // if it's the tenth frame of the game
+//                System.out.println("Total start -> " + totalScore);
+//                if (scores[activeFrameIndex] == 10 && scores[activeFrameIndex + 1] == 10) {
+//                    totalScore += scores[activeFrameIndex + 2];
+//                } if (scores[activeFrameIndex] == 10) {
+//                    totalScore += scores[activeFrameIndex + 1] + scores[activeFrameIndex + 2];
+//                } else if ((scores[activeFrameIndex] + scores[activeFrameIndex + 1] == 10)) {
+//                    totalScore += scores[activeFrameIndex + 2];
+//                } else if (scores[activeFrameIndex + 1] == 10) {
+//                    totalScore += scores[activeFrameIndex + 2];
+//                }
+//
+//                totalScore += scores[activeFrameIndex + 2];
+//
+//                System.out.println("Total end -> " + totalScore);
+//
+//            }
+//
+//            previousFrameScore = score;
+//
+//            totalScore += score;
         }
 
         return totalScore;
+    }
+
+    private int getNextFrameIndex(int frameIndex){
+        return frameIndex + 2;
     }
 }
